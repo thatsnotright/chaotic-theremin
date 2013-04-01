0dfc625ebc224619a02ab4b82e07b4bb2668eb81
Rob Elsner <rob@derpitude.(none)>
2013-04-01 14:44:08 -0600

    Add initial overtone files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to chaotic-theremin
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/chaotic_theremin/core_test.clj
@@ -0,0 +1,7 @@
+(ns chaotic-theremin.core-test
+  (:use clojure.test
+        chaotic-theremin.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
