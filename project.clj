522f2972eb0e35b1338bb2a048f84ea32b4d04e9
Rob <rob@macitude.(none)>
2013-04-16 19:17:50 -0600

    Abandoning generating the trajectories just gonna read from file

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.8.1" :exclusions [org.clojure/clojure]]]
+                 [overtone "0.8.1" :exclusions [org.clojure/clojure]]
+                 [konato/konato "1.0.0"]
+                ]
+:plugins [[lein-localrepo "0.4.1"]]
   :aot [chaotictheremin.core]
   :main chaotictheremin.core)
--- src/chaotictheremin/core.clj
+++ src/chaotictheremin/core.clj
@@ -1,25 +1,61 @@
 (ns chaotictheremin.core
-  (:require [chaotictheremin.rk4 :as rk4])
   (:gen-class))
+(use 'com.konato.ode)
+(use 'clojure.java.io)
+(defn savedata
+  "Copyright (c) 2009 Stephane Rousseau (stephaner konato.com). All rights reserved.
 
-(use 'chaotictheremin.rk4)
-(defn pendulum []
-  "Test the Runge-Kutta order 4 method for a classic pendulum dtheta/dt=omega,domega/dt=(-g/L)sin(theta),L=1m,theta(0)=20degrees,omega(0)=0"
-  (let [sigma 10
-        beta (/ 8 3)
-        rho 28
+  The use and distribution terms for this software are covered by the Eclipse Public License 1.0,
+  which can be found in the file epl-v10.html at the root of this distribution.
+  By using this software in any fashion, you are agreeing to be bound by the terms of this license.
+  You must not remove this notice, or any other, from this software."
+  [filename & more]
+  (loop [
+          sqs more
+          onetime true
+          ]
+    (if (empty? (first sqs))
+      nil
+      (let [toprn (map #(first %) sqs)]
+        (if onetime
+          (with-open [wrtr (writer filename)]
+            (doall
+              (map #(.write wrtr (str %))
+                (interpose " " toprn)))
+            (.write wrtr "\n"));delete first
+          (with-open [wrtr (writer filename :append true)]
+            (doall
+              (map #(.write wrtr (str %))
+                (interpose " " toprn)))
+            (.write wrtr "\n")))
+        (recur (map #(rest %)  sqs) false))))
+  )
+
+(defn lorenz []
+  (let [sigma 28.0
+        beta 4
+        rho 46.92
         T0 0.0
-        N 30
-        DT 0.001
-        fps  {:x #(* sigma (- (:y %) (:x %))),
-              :y #(- (* (:x %) (- rho (:z %)) (:y %))),
-              :z #(- (* (:x %) (:y %)) (- beta (:z %)))}
+        N 3
+        DT 0.01
+        fps {
+              :xdot #(* sigma (- (:y %) (:x %))),
+              :ydot #(- (* (:x %) (- rho (:z %))) (:y %)),
+              :zdot #(- (* (:x %) (:y %)) (* beta (:z %)))
+              :x #(:xdot %),
+              :z #(:zdot %),
+              :y #(:ydot %),
+              }
         outfps {:x #(:x %), :y #(:y %), :z #(:z %), :t #(:t %)}
         ]
-    (rk4/odesolve rk4 fps {:x 0.0, :y 0.01 :z 0.01} :t T0 N DT outfps)
+    (odesolve rk4 fps {:xt 0.0, :yt 0.0, :zt 0.0, :x 0.0, :y 1.0, :z 0.0, :xdot 0.0, :ydot 1.0, :zdot 0.0 } :t T0 N DT outfps)
     ))
 
 (defn -main [& args]
-  (let [a (pendulum)]
-    (doseq [item (apply mapv vector [(:x a) (:y a)])] (prn (clojure.string/join "," item))))
+  (let
+    [a (lorenz)]
+  ;[a (dampedho 1 2 100 0.9 0.0 0.0 2.0 0.01)]
+    ;(doseq [item (apply mapv vector [(:x a)  (:t a)])] (println (format "%.6f,%.6f" (nth item 0) (nth item 1)))))
+    (savedata "d.csv" (:t a) (:x a) (:y a) (:z a))
+    )
   )
